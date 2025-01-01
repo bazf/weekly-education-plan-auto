91e089d787a11d5d58b28291d1eba0d308721e41
Baz <vaquuf@gmail.com>
2025-01-01 15:05:07 +0100

    icons

--- serviceworker.js
+++ serviceworker.js
@@ -8,14 +8,7 @@ const urlsToCache = [
   '/manifest.json',
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.2.0/crypto-js.min.js',
-  '/icons/icon-72x72.png',
-  '/icons/icon-96x96.png',
-  '/icons/icon-128x128.png',
-  '/icons/icon-144x144.png',
-  '/icons/icon-152x152.png',
-  '/icons/icon-192x192.png',
-  '/icons/icon-384x384.png',
-  '/icons/icon-512x512.png'
+  '/icons/icon-152x152.png'
 ];
 
 // Install Service Worker
